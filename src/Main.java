6541bc68ee380cf1a042b5df48f429203d6f860d
isabelle9912 <isabellesaahirah28@gmail.com>
2025-02-06 10:25:44 -0300

    feat: faz a classe de execução com código teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import models.Client;
+import models.Rent;
+import models.Vehicle;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Client client = new Client("João Silva", "123.456.789-00", "99999-9999");
+        Vehicle vehicle = new Vehicle("Toyota Corolla", "ABC-1234", 2022);
+
+        LocalDate start = LocalDate.of(2024, 9, 1);  // Data de início
+        LocalDate end = LocalDate.of(2024, 9, 5);  // Data de devolução
+
+        Rent aluguel = new Rent(client, vehicle, start, end);
+        aluguel.showDetails();
     }
 }
